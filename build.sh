e2030509c665254655d8742622ef3eae5d9edbfa
Andrew Chambers <andrewchamberss@gmail.com>
2016-10-13 21:05:30 +1300

    add skeleton

---
+++ build.sh
@@ -0,0 +1,12 @@
+set -e
+
+for FILE in $GOPATH/src/github.com/buppyio/bpy/doc/*.md
+do
+    OUT=docroot/`basename -s .md $FILE`.html
+    cat doc.head.html > $OUT
+    pandoc -f markdown -t html < $FILE >> $OUT
+    cat doc.tail.html >> $OUT
+    
+    TITLE=`basename $FILE | sed 's/.\([0-9]\).md/(\1)/g'`
+    sed -i "s/{{TITLE}}/$TITLE/g" $OUT
+done
