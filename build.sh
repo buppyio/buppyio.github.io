fbcc79ec4b1489ed2eb46079ddc70dd41142232d
Andrew Chambers <andrewchamberss@gmail.com>
2016-11-02 19:55:19 +1300

    man pages

--- build.sh
+++ build.sh
@@ -1,12 +1,30 @@
 set -e
 
-for FILE in $GOPATH/src/github.com/buppyio/bpy/doc/*.md
+buildpage () {
+	IN=$1
+	CATEGORY=$2
+	NAME=`basename -s .md $FILE`
+	OUT=./man/$CATEGORY/$NAME.html
+	TITLE="$NAME($CATEGORY)"
+	
+	cat doc.head.html > $OUT
+	pandoc -f markdown -t html < $IN >> $OUT
+	cat doc.tail.html >> $OUT
+	sed -i "s/{{TITLE}}/$TITLE/g" $OUT
+}
+
+if test -e ./man
+then
+	rm -rf ./man
+fi
+
+mkdir ./man
+
+for CATEGORY in `ls $GOPATH/src/github.com/buppyio/bpy/doc/man/`
 do
-    OUT=`basename -s .md $FILE`.html
-    cat doc.head.html > $OUT
-    pandoc -f markdown -t html < $FILE >> $OUT
-    cat doc.tail.html >> $OUT
-    
-    TITLE=`basename $FILE | sed 's/.\([0-9]\).md/(\1)/g'`
-    sed -i "s/{{TITLE}}/$TITLE/g" $OUT
+	mkdir ./man/$CATEGORY/
+	for FILE in $GOPATH/src/github.com/buppyio/bpy/doc/man/$CATEGORY/*.md
+	do
+		buildpage $FILE $CATEGORY
+	done
 done
